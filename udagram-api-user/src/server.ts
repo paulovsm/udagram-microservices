60ecba137900c562f86792c751cb29ca7253fed9
Paulo Victor Moura <paulovsm@gmail.com>
2021-01-28 23:24:53 -0300

    Change Frontend Docker Image and Deployment

--- udagram-api-user/src/server.ts
+++ udagram-api-user/src/server.ts
@@ -25,14 +25,9 @@ import {V0_USER_MODELS} from './controllers/v0/model.index';
       'X-Access-Token', 'Authorization',
     ],
     methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    origin: config.url,
+    //origin: config.url,
+    origin: '*',
   }));
-
-  app.use(function(req, res, next) {     
-    res.header("Access-Control-Allow-Origin", "http://localhost:8100");     
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');     
-    next();  
-  });
 
   app.use('/api/v0/', IndexRouter);
 
--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -29,12 +29,6 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
     origin: config.url,
   }));
 
-  app.use(function(req, res, next) {     
-    res.header("Access-Control-Allow-Origin", "http://localhost:8100");     
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');     
-    next();  
-  });
-
   app.use('/api/v0/', IndexRouter);
 
   // Root URI call
